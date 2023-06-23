316b2ba5ba990e632a6829a469f68b35f3631156
Junsang Mun <mun.js@aol.com>
2023-06-24 03:49:13 +0900

    feat: add QR code generator feature
    - QRCodeWidget with qr_flutter package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:james25/main.dart';
+import 'package:james25/main.dart.bak';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
