2befa7f88ce7505b43ee1ad359e272c5289e6dd2
Junsang Mun <mun.js@aol.com>
2023-06-26 18:25:20 +0900

    [WIP] adding ListView in shopping_cart_page

--- lib/shopping_cart_page.dart
+++ lib/shopping_cart_page.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
-import 'main.dart';
+class ListData {
+  final String product;
+  final int price;
+  final int count;
 
-class ShoppingCartPage extends StatelessWidget {
+  ListData(this.product, this.price, this.count);
+}
+
+class ShoppingCartPage extends StatefulWidget {
   const ShoppingCartPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Column(children: [
-        Text('쑈오핑카트'),
-        ChangeTab(),
-      ],),
-    );
-  }
+  _ShoppingCartPageState createState() => _ShoppingCartPageState();
+}
+
+class _ShoppingCartPageState extends State<ShoppingCartPage> {
+    final List<ListData> datas = [
+    ListData('Swift', 800, 1),
+    ListData('Kotlin', 50000, 2),
+    ListData('Objective-c', 4000, 1),
+    ListData('Flutter', 3000, 2),
+    ListData('JAVA', 2000, 2),
+    ListData('Python', 1000, 1),
+    ListData('PHP', 1000, 2),
+  ];
+
 }
